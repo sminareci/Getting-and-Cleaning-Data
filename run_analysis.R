8dcefc42ba6406a40d24f4444048376b4761f313
Semih Minareci <sminareci@outlook.com>
2016-04-30 21:51:12 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,9 @@
+run_analysis <- function() { 
+      
+    ## Reads list of features and remove illegal characters 
+     ## Referenced from ?make.names in R documentation 
+     features <- read.table("UCI_HAR_Dataset/features.txt", colClasses = "character") 
+     features[,2] <- gsub("\\(|\\)","",features[,2]) 
+     features[,2] <- gsub("-|,","_", features[,2]) 
+     features[,2] <- tolower(features[,2]) 
 
